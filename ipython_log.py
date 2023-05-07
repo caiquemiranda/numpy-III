6986cba6b4e2cce3890d05014a90d1c539a1b681
caiquemiranda <caiquemirandas@gmail.com>
2023-05-07 14:37:58 -0300

    Instalando o Matplotlib

--- ipython_log.py
+++ ipython_log.py
@@ -4,3 +4,5 @@ $ ipython -pylab
 get_ipython().system('$ ipython -pylab')
 get_ipython().system('ipython -pylab')
 get_ipython().run_line_magic('logstart', '')
+get_ipython().system('data')
+get_ipython().system('date')
